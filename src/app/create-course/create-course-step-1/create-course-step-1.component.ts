3880f64be7128bf9c3542318aa3938cfe3c2ea25
EmadERagheb <emaderagheb@gmail.com>
2024-06-21 17:16:04 +0300

    form components

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,33 +1,35 @@
-import {Component} from '@angular/core';
-import {UntypedFormBuilder, Validators} from '@angular/forms';
-
+import { Component } from "@angular/core";
+import { UntypedFormBuilder, Validators } from "@angular/forms";
+import { MatCalendarCellClassFunction } from "@angular/material/datepicker";
 
 @Component({
   selector: "create-course-step-1",
-  templateUrl:"create-course-step-1.component.html",
-  styleUrls: ["create-course-step-1.component.scss"]
+  templateUrl: "create-course-step-1.component.html",
+  styleUrls: ["create-course-step-1.component.scss"],
 })
 export class CreateCourseStep1Component {
-
+  dataClass: MatCalendarCellClassFunction<Date> = (cellData, view) => {
+    const data = cellData.getDate();
+    if (view == "month") {
+      return data == 1 ? 'highlight-data' : '';
+    }
+    return "";
+  };
   form = this.fb.group({
-    title: ['', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(60)
-    ]],
+    title: [
+      "",
+      [Validators.required, Validators.minLength(5), Validators.maxLength(60)],
+    ],
     releasedAt: [new Date(), Validators.required],
-    category: ['BEGINNER', Validators.required],
-    courseType: ['premium', Validators.required],
+    category: ["BEGINNER", Validators.required],
+    courseType: ["premium", Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: ["", [Validators.required, Validators.minLength(3)]],
   });
 
-  constructor(private fb: UntypedFormBuilder) {
-
-  }
+  constructor(private fb: UntypedFormBuilder) {}
 
   get courseTitle() {
-    return this.form.controls['title'];
+    return this.form.controls["title"];
   }
-
 }
